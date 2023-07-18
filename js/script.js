5fee4d23e0b8d2ef8bef5659959626a72b78cae5
Choiyubin <polkmn0995@gmail.com>
2023-07-18 17:58:34 +0900

    footer 부분까지 기초 작업 + css 깔끔하게 수정

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 window.onload = function () {
-  //AOS셋팅
+  //AOS 셋팅
   AOS.init();
-  // 상단 스크롤 기능
+  //상단 스크롤 기능
   const header = document.querySelector(".header");
   const mbt = document.querySelector(".mbt");
   let scy = 0;
-  // 1.스크롤바의 픽셀 위치값을 파악해서
+  //1.스크롤바의 픽셀 위치값을 파악해서
   scy = window.document.documentElement.scrollTop;
-  // 2.class적용
+  //2.class적용
   if (scy > 0) {
     header.classList.add("active");
     mbt.classList.add("active");
@@ -18,6 +18,7 @@ window.onload = function () {
       header.classList.add("active");
       mbt.classList.add("active");
     } else {
+      //스크롤이 되지 않은 상태이면서!
       const state = navMb.classList.contains("active");
       if (state) {
         // 만약에 모바일 메뉴가 펼쳐진 상태라면
@@ -30,17 +31,17 @@ window.onload = function () {
       }
     }
   });
-  // 모바일 메뉴 클릭 처리
+  //모바일 메뉴 클릭 처리
   const htmlRoot = document.querySelector("html");
   const navMb = document.querySelector(".nav-mb");
   mbt.addEventListener("click", function () {
-    // 현재 ani클래스가 있는지 없는지 파악
+    //현재 ani클래스가 있는지 없는지 파악
     const state = this.classList.contains("ani");
     if (state) {
       this.classList.remove("ani");
-      // 윈도우에 스크롤바가 나타납니다.
+      //윈도우에 스크롤바가 나타납니다.
       htmlRoot.classList.remove("active");
-      // 모바일 메뉴 숨기기
+      //모바일 메뉴 숨기기
       navMb.classList.remove("active");
       if (scy > 0) {
         header.classList.add("active");
@@ -57,45 +58,39 @@ window.onload = function () {
       mbt.classList.add("active");
     }
   });
-  // 반응형 처리
+  //반응형 처리
   let winW = window.innerWidth;
   window.addEventListener("resize", function () {
-    // 웹브라우저 안쪽 너비
     winW = window.innerWidth;
-    // mobile ===> pc 전환
     if (winW > 1024) {
       mbt.classList.remove("ani");
       htmlRoot.classList.remove("active");
       navMb.classList.remove("active");
-
       if (scy > 0) {
-        // 스크롤이 된 상태에서 화면 리사이징..
         header.classList.add("active");
         mbt.classList.add("active");
       } else {
-        // 스크롤 안됨. 화면 리사이징..
         header.classList.remove("active");
         mbt.classList.remove("active");
       }
     }
   });
-  // 비주얼 슬라이드
-  // 1.슬라이드 (.swiper-slide)개수 만큼 li생성하기
+  //비주얼 슬라이드
+  //1.슬라이드(.swiper-slide)개수 만큼 li 생성하기
   const swSlideCount = document.querySelectorAll(
     ".sw-visual .swiper-slide"
   ).length;
-  // 2.li태그 출력 장소(ul태그 )저장
+  //2. li태그 출력 장소(ul 태그)저장
   const swSlidePgUl = document.querySelector(".sw-visual-pg-list");
-  //3.li에 html로 작성할 글자를 생성한다.
+  //3. li에 html로 작성할 글자를 생성한다.
   let swVisualHtml = ``;
   for (let i = 0; i < swSlideCount; i++) {
     swVisualHtml = swVisualHtml + `<li>${i + 1}</li>`;
   }
-  // 4.html을 추가한다
+  //4.html을 추가한다.
   swSlidePgUl.innerHTML = swVisualHtml;
-  // 5.페이지네이션 관련(코딩으로 생성한 li태그 저장)
+  //5.페이지네이션 관련(코딩으로 생성한 li태그 저장)
   const swVisualPgLi = document.querySelectorAll(".sw-visual-pg-list > li");
-  console.log(swVisualPgLi);
 
   var swiper = new Swiper(".sw-visual", {
     effect: "fade",
@@ -117,27 +112,45 @@ window.onload = function () {
       prevEl: ".sw-visual-prev",
     },
   });
-  // swiper 가 최초 실행될때
-  // 1번 li 의 흰색 라인이 늘어나는 모션을 실행
+  //swiper가 최초 실행될때
+  //1번 li 의 흰색 라인이 늘어나는 모션을 실행
   swVisualPgLi[0].classList.add("active");
-  // swiper가 바뀔때 마다 실행
-  // 슬라이더가 바뀌는 상태를 찾아서
-  // 우리가 적용하고자 하는 처리를 하고
-  // swiper의 API를 참조해서 작성
+  //swiper가 바뀔때 마다 실행
+  //슬라이더가 바뀌는 상태를 찾아서
+  //우리가 적용하고자 하는 처리를 하고
+  //swiper의 API를 참조해서 작성
   swiper.on("slideChange", function () {
     swVisualPgLi.forEach(function (item, index) {
       if (swiper.realIndex === index) {
-        // realIndex는 진짜 html태그의 순서값
+        //realIndex 는 진짜 html 태그의 순서값
         item.classList.add("active");
       } else {
         item.classList.remove("active");
       }
     });
   });
-  //li태그를 클릭을 하면 처리하기
+  //li태그를 클릭을 학면 처리하기
   swVisualPgLi.forEach(function (item, index) {
     item.addEventListener("click", function () {
       swiper.slideToLoop(index, 500, false);
     });
+  });
+  // business slide
+  const swBusiness = new Swiper(".sw-business", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  // business-modal 기능
+  const businessModal = document.querySelector(".business-modal");
+  businessModal.addEventListener("click", function () {
+    businessModal.style.display = "none";
   });
 };
